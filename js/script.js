ad9a0b372977b99960761beff5d18bee918eb19b
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-04-22 17:26:18 +0200

    completed

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		todos: [
+			{
+				text: "guardare lezione",
+				completed: false,
+			},
+			{
+				text: "finire lezione",
+				completed: false,
+			},
+			{
+				text: "allenarsi",
+				completed: false,
+			},
+			{
+				text: "guadagnare bitcoin",
+				completed: false,
+			},
+		],
+		newToDo: " ",
+	},
+	methods: {
+		addToDo() {
+			// console.log("add new item");
+
+			if (this.newToDo !== " ") {
+				this.todos.push({
+					text: this.newToDo,
+					completed: false,
+				});
+
+				this.newToDo = "";
+				this.$refs.todoInput.focus();
+			}
+		},
+
+		removeToDo(index) {
+			this.todos.splice(index, 1);
+		},
+
+		updateStatus(index) {
+			this.todos[index].completed = !this.todos[index].completed;
+		},
+	},
+});
